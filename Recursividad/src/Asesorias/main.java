090296e9ead986cc192ec8e2391547ec327d3f25
Marvin Moises Avila Lugardo <marvin.200moy@gmail.com>
2022-11-25 22:47:21 -0600

    si

--- Recursividad/src/Asesorias/main.java
+++ Recursividad/src/Asesorias/main.java
@@ -1,11 +1,46 @@
 package Asesorias;
 
+import javax.swing.JOptionPane;
+
+import HolaMundoRI.divicion;
+
 public class main {
  public static void main(String[] args) {
-    
-    Rasesorias obj =new Rasesorias();
+   
+    int uno =0;
+    int dos=0;
 
-    obj.imprimir(50);
+    int re;
+
+        
+
+        try {
+             re = uno / dos;
+            System.out.println(re);
+        } catch (ArithmeticException e) {
+            System.out.println("no se puede dividir ");
+        }
+
+   /*  int edad=22;
+    String mayor = 
+    edad>=18 && edad<=20?"es mayor":"es menor";
+    System.out.println(mayor);
+
+
+
+    if (edad>=18 ) {
+        System.out.println("es mayor");
+    }else{
+        System.out.println("es menor");
+    }
+ */
+
+    
+    
+/* 
+   for (int i = 0; i < 10; i++) {
+        System.out.println(i);
+   } */
  }   
 }
 
